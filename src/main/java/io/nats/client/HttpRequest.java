nats-io/nats.java

Adds websocket client support, resolves #399

src/main/java/io/nats/client/HttpRequest.java
@@ -1,0 +1,119 @@
+// Copyright 2021 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package io.nats.client;
+
+/**
+ * Encapsulate an HttpRequest, in Java 11 we could use this class:
+ * https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpRequest.html
+ * 
+ * ...but we want to support older JVMs.
+ */
+public class HttpRequest {
+    private String method = "GET";
+    private String uri = "/";
+    private String version = "1.0";
+    private HttpHeaders headers = new HttpHeaders();
+
+    /**
+     * @return the attached http headers, defaults to GET
+     */
+    public HttpHeaders getHeaders() {
+        return headers;
+    }
+
+    /**
+     * @return the request method (GET, POST, etc)
+     */
+    public String getMethod() {
+        return method;
+    }
+
+    /**
+     * Note that no validation is performed, but the method is trimmed of whitespace
+     * and converted to all upper case.
+     * 
+     * @param method is the new request method to use.
+     * @return this for method chaining.
+     */
+    public HttpRequest method(String method) {
+        if (null == method) {
+            throw new IllegalArgumentException("HttpRequest method must be non-null");
+        }
+        this.method = method.trim().toUpperCase();
+        return this;
+    }
+
+    /**
+     * This is the RAW URI, you may need to perform URL decoding the result.
+     * 
+     * @return the "path" of the URI (in the RFC this is the request-URI)
+     */
+    public String getURI() {
+        return uri;
+    }
+
+    /**
+     * This sets the RAW URI, you may need to perform URL encoding before passing
+     * into this method.
+     * 
+     * @param uri is the new "path" of the URI to use.
+     * @return this for method chaining.
+     */
+    public HttpRequest uri(String uri) {
+        if (null == uri) {
+            throw new IllegalArgumentException("HttpRequest uri must be non-null");
+        }
+        this.uri = uri;
+        return this;
+    }
+
+    /**
+     * @return the HTTP version to use for this request. Defaults to "1.0"
+     */
+    public String getVersion() {
+        return version;
+    }
+
+    /**
+     * Note that no validation is performed on the version. Probably only makes
+     * sense to use "0.9", "1.0", "1.1", or "2".
+     * 
+     * @param version is the new HTTP version to use.
+     * @return this for method chaining.
+     */
+    public HttpRequest version(String version) {
+        if (null == version) {
+            throw new IllegalArgumentException("HttpRequest version must be non-null");
+        }
+        this.version = version;
+        return this;
+    }
+
+    /**
+     * @return the textual representation of the HTTP request line + headers (no
+     *         body)
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(method);
+        sb.append(" ");
+        sb.append(uri);
+        sb.append(" HTTP/");
+        sb.append(version);
+        sb.append("\r\n");
+        sb.append(headers.toString());
+        return sb.toString();
+    }
+}

src/main/java/io/nats/client/impl/WebsocketOutputStream.java
@@ -1,0 +1,115 @@
+// Copyright 2021 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package io.nats.client.impl;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.security.SecureRandom;
+import java.util.Random;
+
+import io.nats.client.impl.WebsocketFrameHeader.OpCode;
+
+public class WebsocketOutputStream extends OutputStream {
+    private OutputStream wrap;
+    private boolean masked;
+    private byte[] oneByte = new byte[1];
+    /**
+     * Minimize fragmenting, by using a 1440 buffer size. This buffer is used to
+     * store the websocket header + payload for the first write.
+     *
+     * We are using this size since a typical MTU for ethernet is 1500 and the
+     * TCP/IPv6 header consumes 60 bytes, thus by using 1440 we should minimize
+     * the need for further fragmentation at the lower layers and yet minimize
+     * the overhead associated with the fact that a write to the Socket (which
+     * has NO_DELAY set on it) results in a single TCP/IPv6 datagram which has
+     * a 60 byte overhead.
+     *
+     * Note that the effective payload is less due to the websocket frame overhead:
+     * 4 bytes header/length + 4 bytes masking. Which would be a payload
+     * size of 1432. This means any write which is larger than 1432 will be
+     * fragmented into two socket writes and will thus have another 60 bytes
+     * of TCP/IPv6 datagram header overhead.
+     */
+    private byte[] headerBuffer = new byte[1440];
+    private WebsocketFrameHeader header = new WebsocketFrameHeader()
+        .withOp(OpCode.BINARY, true)
+        .withNoMask();
+    private Random random = new SecureRandom();
+
+    public WebsocketOutputStream(OutputStream wrap, boolean masked) {
+        this.wrap = wrap;
+        this.masked = masked;
+    }
+
+    @Override
+    public void close() throws IOException {
+        // NOTE: Per spec, we should technically wait to receive the close frame,
+        // but we are not in control of the InputStream here...
+        WebsocketFrameHeader header = new WebsocketFrameHeader()
+            .withOp(OpCode.CLOSE, true)
+            .withNoMask()
+            .withPayloadLength(0);
+        int length = header.read(headerBuffer, 0, headerBuffer.length);
+        wrap.write(headerBuffer, 0, length);
+        wrap.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        wrap.flush();
+    }
+
+    @Override
+    public void write(byte[] buffer) throws IOException {
+        write(buffer, 0, buffer.length);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        oneByte[0] = (byte)(b & 0xFF);
+        write(oneByte, 0, 1);
+    }
+
+    /**
+     * NOTE: the buffer will be modified if masking is enabled and the length is greater
+     * than 1432. Regardless of if masking is enabled or not, any writes of length > 1432
+     * will be split into two writes to the underlying OutputStream which is being wrapped.
+     */
+    @Override
+    public void write(byte[] buffer, int offset, int length) throws IOException {
+        header.withPayloadLength(length);
+        if (masked) {
+            header.withMask(random.nextInt());
+        }
+        int headerBufferOffset = header.read(headerBuffer, 0, headerBuffer.length);
+        int consumed = Math.min(length, headerBuffer.length - headerBufferOffset);
+        System.arraycopy(buffer, offset, headerBuffer, headerBufferOffset, consumed);
+
+        header.filterPayload(headerBuffer, headerBufferOffset, consumed);
+        wrap.write(headerBuffer, 0, headerBufferOffset + consumed);
+        if (consumed < length) {
+            // NOTE: We could perform a "mark" operation before filtering the
+            // payload which saves the payloadLength and maskingKeyOffset, then
+            // perform a "resetMark" operation after writing the masked buffer
+            // and finally re-applying the filter in order to preserve the
+            // original values of the bytes. However, there appears to be no
+            // code which relies on the bytes in the buffer to be preserved,
+            // so we are keeping things efficient by not performing these
+            // operations.
+            header.filterPayload(buffer, offset + consumed, length - consumed);
+            wrap.write(buffer, offset + consumed, length - consumed);
+        }
+    }
+
+}

src/test/java/io/nats/client/HttpRequestTests.java
@@ -1,0 +1,67 @@
+// Copyright 2015-2018 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package io.nats.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class HttpRequestTests {
+    @Test
+    public void testDefaults() {
+        HttpRequest request = new HttpRequest();
+        assertEquals("GET", request.getMethod());
+        assertEquals("/", request.getURI());
+        assertEquals("1.0", request.getVersion());
+        assertEquals(new HttpHeaders(), request.getHeaders());
+        assertEquals(
+            "GET / HTTP/1.0\r\n" +
+            "\r\n",
+            request.toString());
+    }
+
+    @Test
+    public void testSetters() {
+        HttpRequest request = new HttpRequest()
+            .method("PUT")
+            .uri("/fun")
+            .version("1.1");
+        request.getHeaders()
+            .add("One", "1")
+            .add("Two", "2");
+        assertEquals("PUT", request.getMethod());
+        assertEquals("/fun", request.getURI());
+        assertEquals("1.1", request.getVersion());
+        assertEquals(
+            "PUT /fun HTTP/1.1\r\n" +
+            "One: 1\r\n" +
+            "Two: 2\r\n" +
+            "\r\n",
+            request.toString());
+    }
+
+    @Test
+    public void testNulls() {
+        HttpRequest request = new HttpRequest();
+        assertThrows(IllegalArgumentException.class, () -> {
+            request.method(null);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            request.uri(null);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            request.version(null);
+        });
+    }
+}

nats-io/jnats

src/main/java/io/nats/client/HttpRequest.java
@@ -1,0 +1,129 @@
+// Copyright 2021 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package io.nats.client;
+
+import io.nats.client.impl.Headers;
+
+/**
+ * Encapsulate an HttpRequest, in Java 11 we could use this class:
+ * https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpRequest.html
+ * 
+ * ...but we want to support older JVMs.
+ */
+public class HttpRequest {
+    private String method = "GET";
+    private String uri = "/";
+    private String version = "1.0";
+    private Headers headers = new Headers();
+
+    /**
+     * @return the attached http headers, defaults to GET
+     */
+    public Headers getHeaders() {
+        return headers;
+    }
+
+    /**
+     * @return the request method (GET, POST, etc)
+     */
+    public String getMethod() {
+        return method;
+    }
+
+    /**
+     * Note that no validation is performed, but the method is trimmed of whitespace
+     * and converted to all upper case.
+     * 
+     * @param method is the new request method to use.
+     * @return this for method chaining.
+     */
+    public HttpRequest method(String method) {
+        if (null == method) {
+            throw new IllegalArgumentException("HttpRequest method must be non-null");
+        }
+        this.method = method.trim().toUpperCase();
+        return this;
+    }
+
+    /**
+     * This is the RAW URI, you may need to perform URL decoding the result.
+     * 
+     * @return the "path" of the URI (in the RFC this is the request-URI)
+     */
+    public String getURI() {
+        return uri;
+    }
+
+    /**
+     * This sets the RAW URI, you may need to perform URL encoding before passing
+     * into this method.
+     * 
+     * @param uri is the new "path" of the URI to use.
+     * @return this for method chaining.
+     */
+    public HttpRequest uri(String uri) {
+        if (null == uri) {
+            throw new IllegalArgumentException("HttpRequest uri must be non-null");
+        }
+        this.uri = uri;
+        return this;
+    }
+
+    /**
+     * @return the HTTP version to use for this request. Defaults to "1.0"
+     */
+    public String getVersion() {
+        return version;
+    }
+
+    /**
+     * Note that no validation is performed on the version. Probably only makes
+     * sense to use "0.9", "1.0", "1.1", or "2".
+     * 
+     * @param version is the new HTTP version to use.
+     * @return this for method chaining.
+     */
+    public HttpRequest version(String version) {
+        if (null == version) {
+            throw new IllegalArgumentException("HttpRequest version must be non-null");
+        }
+        this.version = version;
+        return this;
+    }
+
+    /**
+     * @return the textual representation of the HTTP request line + headers (no
+     *         body)
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(method);
+        sb.append(" ");
+        sb.append(uri);
+        sb.append(" HTTP/");
+        sb.append(version);
+        sb.append("\r\n");
+        headers.forEach((key, values) -> {
+            values.stream().forEach(value -> {
+                sb.append(key);
+                sb.append(": ");
+                sb.append(value);
+                sb.append("\r\n");
+            });
+        });
+        sb.append("\r\n");
+        return sb.toString();
+    }
+}

src/main/java/io/nats/client/support/WebsocketOutputStream.java
@@ -1,0 +1,115 @@
+// Copyright 2021 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package io.nats.client.support;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.security.SecureRandom;
+import java.util.Random;
+
+import io.nats.client.support.WebsocketFrameHeader.OpCode;
+
+public class WebsocketOutputStream extends OutputStream {
+    private OutputStream wrap;
+    private boolean masked;
+    private byte[] oneByte = new byte[1];
+    /**
+     * Minimize fragmenting, by using a 1440 buffer size. This buffer is used to
+     * store the websocket header + payload for the first write.
+     *
+     * We are using this size since a typical MTU for ethernet is 1500 and the
+     * TCP/IPv6 header consumes 60 bytes, thus by using 1440 we should minimize
+     * the need for further fragmentation at the lower layers and yet minimize
+     * the overhead associated with the fact that a write to the Socket (which
+     * has NO_DELAY set on it) results in a single TCP/IPv6 datagram which has
+     * a 60 byte overhead.
+     *
+     * Note that the effective payload is less due to the websocket frame overhead:
+     * 4 bytes header/length + 4 bytes masking. Which would be a payload
+     * size of 1432. This means any write which is larger than 1432 will be
+     * fragmented into two socket writes and will thus have another 60 bytes
+     * of TCP/IPv6 datagram header overhead.
+     */
+    private byte[] headerBuffer = new byte[1440];
+    private WebsocketFrameHeader header = new WebsocketFrameHeader()
+        .withOp(OpCode.BINARY, true)
+        .withNoMask();
+    private Random random = new SecureRandom();
+
+    public WebsocketOutputStream(OutputStream wrap, boolean masked) {
+        this.wrap = wrap;
+        this.masked = masked;
+    }
+
+    @Override
+    public void close() throws IOException {
+        // NOTE: Per spec, we should technically wait to receive the close frame,
+        // but we are not in control of the InputStream here...
+        WebsocketFrameHeader header = new WebsocketFrameHeader()
+            .withOp(OpCode.CLOSE, true)
+            .withNoMask()
+            .withPayloadLength(0);
+        int length = header.read(headerBuffer, 0, headerBuffer.length);
+        wrap.write(headerBuffer, 0, length);
+        wrap.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        wrap.flush();
+    }
+
+    @Override
+    public void write(byte[] buffer) throws IOException {
+        write(buffer, 0, buffer.length);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        oneByte[0] = (byte)(b & 0xFF);
+        write(oneByte, 0, 1);
+    }
+
+    /**
+     * NOTE: the buffer will be modified if masking is enabled and the length is greater
+     * than 1432. Regardless of if masking is enabled or not, any writes of length &gt; 1432
+     * will be split into two writes to the underlying OutputStream which is being wrapped.
+     */
+    @Override
+    public void write(byte[] buffer, int offset, int length) throws IOException {
+        header.withPayloadLength(length);
+        if (masked) {
+            header.withMask(random.nextInt());
+        }
+        int headerBufferOffset = header.read(headerBuffer, 0, headerBuffer.length);
+        int consumed = Math.min(length, headerBuffer.length - headerBufferOffset);
+        System.arraycopy(buffer, offset, headerBuffer, headerBufferOffset, consumed);
+
+        header.filterPayload(headerBuffer, headerBufferOffset, consumed);
+        wrap.write(headerBuffer, 0, headerBufferOffset + consumed);
+        if (consumed < length) {
+            // NOTE: We could perform a "mark" operation before filtering the
+            // payload which saves the payloadLength and maskingKeyOffset, then
+            // perform a "resetMark" operation after writing the masked buffer
+            // and finally re-applying the filter in order to preserve the
+            // original values of the bytes. However, there appears to be no
+            // code which relies on the bytes in the buffer to be preserved,
+            // so we are keeping things efficient by not performing these
+            // operations.
+            header.filterPayload(buffer, offset + consumed, length - consumed);
+            wrap.write(buffer, offset + consumed, length - consumed);
+        }
+    }
+
+}

src/test/java/io/nats/client/HttpRequestTests.java
@@ -1,0 +1,69 @@
+// Copyright 2015-2018 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package io.nats.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import io.nats.client.impl.Headers;
+
+public class HttpRequestTests {
+    @Test
+    public void testDefaults() {
+        HttpRequest request = new HttpRequest();
+        assertEquals("GET", request.getMethod());
+        assertEquals("/", request.getURI());
+        assertEquals("1.0", request.getVersion());
+        assertEquals(new Headers(), request.getHeaders());
+        assertEquals(
+            "GET / HTTP/1.0\r\n" +
+            "\r\n",
+            request.toString());
+    }
+
+    @Test
+    public void testSetters() {
+        HttpRequest request = new HttpRequest()
+            .method("PUT")
+            .uri("/fun")
+            .version("1.1");
+        request.getHeaders()
+            .add("One", "1")
+            .add("Two", "2");
+        assertEquals("PUT", request.getMethod());
+        assertEquals("/fun", request.getURI());
+        assertEquals("1.1", request.getVersion());
+        assertEquals(
+            "PUT /fun HTTP/1.1\r\n" +
+            "One: 1\r\n" +
+            "Two: 2\r\n" +
+            "\r\n",
+            request.toString());
+    }
+
+    @Test
+    public void testNulls() {
+        HttpRequest request = new HttpRequest();
+        assertThrows(IllegalArgumentException.class, () -> {
+            request.method(null);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            request.uri(null);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            request.version(null);
+        });
+    }
+}